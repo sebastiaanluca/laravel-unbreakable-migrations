9d340a0f13f111475f278f3493482f925fd5a29e
Sebastiaan Luca <hello@sebastiaanluca.com>
2018-08-06 21:59:32 +0200

    Tweak tests

--- tests/Feature/Commands/GenerateMigrationTest.php
+++ tests/Feature/Commands/GenerateMigrationTest.php
@@ -6,7 +6,10 @@ use SebastiaanLuca\Migrations\Tests\TestCase;
 
 class GenerateMigrationTest extends TestCase
 {
-    public function testItGeneratesACreateTableMigration()
+    /**
+     * @test
+     */
+    public function it generates a create table migration() : void
     {
         $path = __DIR__ . '/../../files/database/migrations';
 
@@ -20,6 +23,8 @@ class GenerateMigrationTest extends TestCase
         ]);
 
         collect(glob($path . '/*create_notifications_table*'))->each(function (string $file) {
+            $this->assertFileExists($file);
+
             $file = file_get_contents($file);
 
             $this->assertContains('class CreateNotificationsTable', $file);
@@ -28,11 +33,14 @@ class GenerateMigrationTest extends TestCase
         });
     }
 
-    public function testItGeneratesAMigrationInAModule()
+    /**
+     * @test
+     */
+    public function it generates a migration in a module() : void
     {
         $path = __DIR__ . '/../../files/modules/MyModule/database/migrations';
 
-        if (! file_exists($path)) {
+        if (! is_dir($path)) {
             mkdir($path, 0777, true);
         }
 
@@ -43,6 +51,8 @@ class GenerateMigrationTest extends TestCase
         ]);
 
         collect(glob($path . '/*create_notifications_table*'))->each(function (string $file) {
+            $this->assertFileExists($file);
+
             $file = file_get_contents($file);
 
             $this->assertContains('class CreateNotificationsTable', $file);
